20afd3c4ffb3ab492668bc1d018279a2847f640d
Jorrit Jongma <git@jongma.org>
2014-01-17 10:41:41 +0100

    Launch SuperSU from Settings
    If SuperSU is installed, show an entry in Settings below
    "Developer options", which launches SuperSU when tapped.
    At boot time SuperSU will detect if com.android.settings
    contains the resources from this patch, and if so it
    will hide itself from the launcher. This was implemented
    in v1.85, so we only show the entry if the installed
    SuperSU version is at least that.
    
    Change-Id: I2af95a1a4396101ff87448cd0ae20addf235af52
    
    Conflicts:
    	res/values/cr_strings.xml
    	res/xml/dashboard_categories.xml
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1337,6 +1337,14 @@ public class SettingsActivity extends Activity
 
                     if (!getPackageManager().hasSystemFeature(
                             CMContextConstants.Features.WEATHER_SERVICES) || !showWeatherMenu) {
+                } else if (id == R.id.supersu_settings) {
+                    // Embedding into Settings is supported from SuperSU v1.85 and up
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
                         removeTile = true;
                     }
                 }
