zhangnl/android_packages_apps_Settings_rr

Settings: Floating Window Fragment

src/com/android/settings/SettingsActivity.java
@@ -75,6 +75,7 @@ import com.android.settings.rr.Navbar;
 import com.android.settings.rr.FlingSettings;
 import com.android.settings.rr.SmartbarSettings;
 import com.android.settings.rr.PulseSettings;
+import com.android.settings.rr.FloatingWindows;
 import com.android.settings.headsup.HeadsUpSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
@@ -337,6 +338,7 @@ public class SettingsActivity extends Activity
 	    GestureAnywhereSettings.class.getName(),	
 	    HeadsUpSettings.class.getName(),	
             PieControl.class.getName(),	
+            FloatingWindows.class.getName(),
             ButtonSettings.class.getName(),
             DisplaySettings.class.getName(),
             DeviceInfoSettings.class.getName(),

src/com/android/settings/rr/FloatingWindows.java
@@ -1,0 +1,107 @@
+package com.android.settings.rr;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.R;
+
+public class FloatingWindows extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        private static final String TAG = "FloatingWindows";
+
+        private static final String FLOATING_WINDOW_MODE = "floating_window_mode";
+        private static final String GESTURE_ANYWHERE_FLOATING = "gesture_anywhere_floating";
+        private static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+
+        SwitchPreference mFloatingWindowMode;
+        SwitchPreference mGestureAnywhereFloatingWindow;
+        SwitchPreference mSlimActionFloatingWindow;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            ContentResolver resolver = getActivity().getContentResolver();
+            Resources res = getResources();
+
+            addPreferencesFromResource(R.xml.floating_windows);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            mFloatingWindowMode = (SwitchPreference)
+                    prefSet.findPreference(FLOATING_WINDOW_MODE);
+            mFloatingWindowMode.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.FLOATING_WINDOW_MODE, 0) == 1);
+            mFloatingWindowMode.setOnPreferenceChangeListener(this);
+
+            mGestureAnywhereFloatingWindow = (SwitchPreference)
+                    prefSet.findPreference(GESTURE_ANYWHERE_FLOATING);
+            mGestureAnywhereFloatingWindow.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_FLOATING, 0) == 1);
+            mGestureAnywhereFloatingWindow.setOnPreferenceChangeListener(this);
+
+            mSlimActionFloatingWindow = (SwitchPreference)
+                    prefSet.findPreference(SLIM_ACTION_FLOATS);
+            mSlimActionFloatingWindow.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.SLIM_ACTION_FLOATS, 0) == 1);
+            mSlimActionFloatingWindow.setOnPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.DONT_TRACK_ME_BRO;
+    }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object objValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            final String key = preference.getKey();
+            if (preference == mFloatingWindowMode) {
+                Settings.System.putInt(resolver,
+                        Settings.System.FLOATING_WINDOW_MODE,
+                (Boolean) objValue ? 1 : 0);
+                return true;
+            } else if (preference == mGestureAnywhereFloatingWindow) {
+                Settings.System.putInt(resolver,
+                        Settings.System.GESTURE_ANYWHERE_FLOATING,
+                (Boolean) objValue ? 1 : 0);
+                return true;
+            } else if (preference == mSlimActionFloatingWindow) {
+                Settings.System.putInt(resolver,
+                        Settings.System.SLIM_ACTION_FLOATS,
+                (Boolean) objValue ? 1 : 0);
+                return true;
+            }
+
+            return true;
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+        }
+}

hydrothunder/android_packages_apps_Settings

src/com/android/settings/SettingsActivity.java
@@ -130,6 +130,7 @@ import com.android.settings.search.DynamicIndexableContentMonitor;
 import com.android.settings.search.Index;
 import com.android.settings.privacyguard.PrivacyGuardPrefs;
 import com.android.settings.sim.SimSettings;
+import com.android.settings.hydrothunder.FloatingWindows;
 import com.android.settings.tts.TextToSpeechSettings;
 import com.android.settings.users.UserSettings;
 import com.android.settings.vpn2.VpnSettings;
@@ -319,6 +320,7 @@ public class SettingsActivity extends Activity
             UserDictionaryList.class.getName(),
             UserDictionarySettings.class.getName(),
             HomeSettings.class.getName(),
+            FloatingWindows.class.getName(),
             DisplaySettings.class.getName(),
             DeviceInfoSettings.class.getName(),
             ManageApplications.class.getName(),

src/com/android/settings/hydrothunder/FloatingWindows.java
@@ -1,0 +1,107 @@
+package com.android.settings.hydrothunder;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.MetricsLogger;
+
+import com.android.settings.R;
+
+public class FloatingWindows extends SettingsPreferenceFragment
+            implements OnPreferenceChangeListener {
+
+        private static final String TAG = "FloatingWindows";
+
+        private static final String FLOATING_WINDOW_MODE = "floating_window_mode";
+        private static final String GESTURE_ANYWHERE_FLOATING = "gesture_anywhere_floating";
+        private static final String SLIM_ACTION_FLOATS = "slim_action_floats";
+
+        SwitchPreference mFloatingWindowMode;
+        SwitchPreference mGestureAnywhereFloatingWindow;
+        SwitchPreference mSlimActionFloatingWindow;
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            ContentResolver resolver = getActivity().getContentResolver();
+            Resources res = getResources();
+
+            addPreferencesFromResource(R.xml.floating_windows);
+
+            PreferenceScreen prefSet = getPreferenceScreen();
+
+            mFloatingWindowMode = (SwitchPreference)
+                    prefSet.findPreference(FLOATING_WINDOW_MODE);
+            mFloatingWindowMode.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.FLOATING_WINDOW_MODE, 0) == 1);
+            mFloatingWindowMode.setOnPreferenceChangeListener(this);
+
+            mGestureAnywhereFloatingWindow = (SwitchPreference)
+                    prefSet.findPreference(GESTURE_ANYWHERE_FLOATING);
+            mGestureAnywhereFloatingWindow.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.GESTURE_ANYWHERE_FLOATING, 0) == 1);
+            mGestureAnywhereFloatingWindow.setOnPreferenceChangeListener(this);
+
+            mSlimActionFloatingWindow = (SwitchPreference)
+                    prefSet.findPreference(SLIM_ACTION_FLOATS);
+            mSlimActionFloatingWindow.setChecked(Settings.System.getInt(resolver,
+                    Settings.System.SLIM_ACTION_FLOATS, 0) == 1);
+            mSlimActionFloatingWindow.setOnPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+        
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.APPLICATION;
+    }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object objValue) {
+            ContentResolver resolver = getActivity().getContentResolver();
+            final String key = preference.getKey();
+            if (preference == mFloatingWindowMode) {
+                Settings.System.putInt(resolver,
+                        Settings.System.FLOATING_WINDOW_MODE,
+                (Boolean) objValue ? 1 : 0);
+                return true;
+            } else if (preference == mGestureAnywhereFloatingWindow) {
+                Settings.System.putInt(resolver,
+                        Settings.System.GESTURE_ANYWHERE_FLOATING,
+                (Boolean) objValue ? 1 : 0);
+                return true;
+            } else if (preference == mSlimActionFloatingWindow) {
+                Settings.System.putInt(resolver,
+                        Settings.System.SLIM_ACTION_FLOATS,
+                (Boolean) objValue ? 1 : 0);
+                return true;
+            }
+
+            return true;
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+        }
+}