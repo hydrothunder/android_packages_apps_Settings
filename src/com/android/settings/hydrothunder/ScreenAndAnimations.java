73dca134244cbcade6b5e20b20a2490650e4da6f
Joshndroid <fmxjoshiewah@gmail.com>
2016-08-08 22:13:51 +1000

    test fix

--- src/com/android/settings/hydrothunder/ScreenAndAnimations.java
+++ src/com/android/settings/hydrothunder/ScreenAndAnimations.java
@@ -1,13 +1,19 @@
-package com.android.settings.hydrothunder;
+package com.android.settings.temasek;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.content.res.Resources;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.SystemProperties;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settings.R;
-import android.provider.Settings;
 
 import com.android.internal.logging.MetricsLogger;
 
@@ -15,6 +21,8 @@ public class ScreenAndAnimations extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "ScreenAndAnimations";
+
+    private Context mContext;
 
     @Override
     protected int getMetricsCategory() {
@@ -24,7 +32,13 @@ public class ScreenAndAnimations extends SettingsPreferenceFragment implements
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        addPreferencesFromResource(R.xml.hydrothunder_settings);
+        addPreferencesFromResource(R.xml.screen_and_animations);
+
+        ContentResolver resolver = getActivity().getContentResolver();
+        Activity activity = getActivity();
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mContext = getActivity().getApplicationContext();
     }
 
     @Override
@@ -32,7 +46,9 @@ public class ScreenAndAnimations extends SettingsPreferenceFragment implements
         super.onResume();
     }
 
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         return false;
     }
 }
